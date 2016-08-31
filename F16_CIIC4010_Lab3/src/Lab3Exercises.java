5d10c9c7b61ffdbf865e3ac635af94cfe53d2be1
4015b15 <4015b15@nispero.ece.uprm.edu>
2016-08-31 10:43:45 -0400

    Changed the title of the JFrame, the size and location.

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -2,10 +2,10 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Miles Ramos");
-		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(850, 400);
-		myFrame.setSize(200, 200);
-		myFrame.setVisible(true);
+		JFrame Miles= new JFrame("Miles Ramos");
+		Miles.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		Miles.setLocation(850, 400);
+		Miles.setSize(200, 200);
+		Miles.setVisible(true);
 	}
 }
